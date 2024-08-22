c6e00b0d989efc6124c0fd2307c4d46da5edb5c9
Rahul <rahulmathur87@gmail.com>
2024-08-22 21:16:17 +0530

    db movies linked to index.html

--- main.py
+++ main.py
@@ -55,7 +55,10 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with app.app_context():
+        result = db.session.execute(db.select(Movies).order_by(Movies.id))
+        all_movies = result.scalars().all()
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
